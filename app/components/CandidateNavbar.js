a9575a6e087f51c77a49d5b1137b22e994a2ff14
devanshig16 <dvsforge5@gmail.com>
2025-04-28 16:28:45 -0400

    navigation fix

--- app/components/CandidateNavbar.js
+++ app/components/CandidateNavbar.js
@@ -7,7 +7,7 @@ export default function CompanyNavbar() {
   return (
     <nav className="w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-3">
       <div className="max-w-full mx-auto flex flex-wrap justify-between items-center px-6">
-        <Link href="/company/dashboard" className="text-2xl font-bold tracking-wide hover:underline">
+        <Link href="/candidate" className="hover:underline">
           Candidate Dashboard
         </Link>
         <div className="flex space-x-6 text-lg font-medium">
@@ -15,7 +15,7 @@ export default function CompanyNavbar() {
           My Profile
           </Link>
           
-          <Link href="/company/browse" className="hover:underline">
+          <Link href="/candidate/dashboard" className="hover:underline">
             Browse Companies
           </Link>
         </div>
--- app/components/CompanyNavbar.js
+++ app/components/CompanyNavbar.js
@@ -4,15 +4,12 @@ export default function CompanyNavbar() {
   return (
     <nav className="w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-3">
       <div className="max-w-full mx-auto flex flex-wrap justify-between items-center px-6">
-        <Link href="/company/dashboard" className="text-2xl font-bold tracking-wide hover:underline">
-          Company Dashboard
+        <Link href="/company/dashboard" className=" hover:underline">
+          Browese Candidates
         </Link>
         <div className="flex space-x-6 text-lg font-medium">
           <Link href="/company/profile" className="hover:underline">
             Profile
-          </Link>
-          <Link href="/candidate/dashboard" className="hover:underline">
-            Browse Candidates
           </Link>
           <Link href="/company" className="hover:underline">
             Browse Companies
